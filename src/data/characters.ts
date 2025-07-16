3ba86074896688b26f5f242d509c4774012a868d
[dyad] <git@dyad.sh>
2025-07-16 09:19:46 +0300

    [dyad] wrote 1 file(s)

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -5,10 +5,20 @@ export interface Character {
 }
 
 export const CHARACTERS: Character[] = [
-  { id: 'jake', name: 'Jake', image: '/images/jake.png' },
-  { id: 'cyberhawk', name: 'Cyberhawk', image: '/images/cyberhawk.png' },
-  { id: 'olaf', name: 'Olaf', image: '/images/olaf.png' },
-  { id: 'tarquinn', name: 'Tarquinn', image: '/images/tarquinn.png' },
-  { id: 'ivan', name: 'Ivan', image: '/images/ivan.png' },
-  { id: 'snake', name: 'Snake', image: '/images/snake.png' },
+  { id: 'butcher', name: 'Butcher', image: '/images/butcher.jpg' },
+  { id: 'grinder', name: 'Grinder', image: '/images/grinder.jpg' },
+  { id: 'hawk', name: 'Hawk', image: '/images/hawk.jpg' },
+  { id: 'ivan', name: 'Ivan', image: '/images/ivan.jpg' },
+  { id: 'jake', name: 'Jake', image: '/images/jake.jpg' },
+  { id: 'kat', name: 'Kat', image: '/images/kat.jpg' },
+  { id: 'olaf', name: 'Olaf', image: '/images/olaf.jpg' },
+  { id: 'rage', name: 'Rage', image: '/images/rage.jpg' },
+  { id: 'rash', name: 'Rash', image: '/images/rash.jpg' },
+  { id: 'roadkill', name: 'Roadkill', image: '/images/roadkill.jpg' },
+  { id: 'slash', name: 'Slash', image: '/images/slash.jpg' },
+  { id: 'snake', name: 'Snake', image: '/images/snake.jpg' },
+  { id: 'tarquin', name: 'Tarquin', image: '/images/tarquin.jpg' },
+  { id: 'vinnie', name: 'Vinnie', image: '/images/vinnie.jpg' },
+  { id: 'violet', name: 'Violet', image: '/images/violet.jpg' },
+  { id: 'viper', name: 'Viper', image: '/images/viper.jpg' },
 ];
